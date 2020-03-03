d8cc88ff8c90c300f4bb41b46c27925e06c52c6e
PengQiWei <2269718867@qq.com>
2020-03-03 14:28:41 +0800

    first

---
+++ src/test/java/com/accp/pqw/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accp.pqw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
